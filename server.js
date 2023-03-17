7b50a0e413e6e2c38cdd7cdc3e64144d15890f2b
antoniamstr1 <mestrovic.antonia@gmail.com>
2023-03-17 16:52:56 +0100

    set controllers and routes

--- server.js
+++ server.js
@@ -1,53 +1,19 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const dotenv = require("dotenv").config();
 
-//image - how to handle static files
-app.use(express.static('public'));
+const app = express();
 
-/* // This responds with "Hello World" on the homepage
-app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
-})
+const port = process.env.PORT || 5000;
 
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
-})
+app.get("/api/contacts", (req,res) => {
+   //res.send("Get all contacts");
+   //ovako šaljem JSON format
+   res.json({message: "Get all contacts!"})
+   //pass status code
+   //[ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+   res.status(200).json({message: "Get all contacts"});
+});
 
-// This responds a DELETE request for the /del_user page.
-app.delete('/del_user', function (req, res) {
-   console.log("Got a DELETE request for /del_user");
-   res.send('Hello DELETE');
-})
-
-// This responds a GET request for the /list_user page.
-app.get('/list_user', function (req, res) {
-   console.log("Got a GET request for /list_user");
-   res.send('Page Listing');
-})
-
-// This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', function(req, res) {   
-   console.log("Got a GET request for /ab*cd");
-   res.send('Page Pattern Match');
-}) */
-
-
-app.get('/process_get', function (req, res) {
-    // Prepare output in JSON format
-    response = {
-       first_name:req.query.first_name,
-       last_name:req.query.last_name
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
- })
- 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(port, () => {
+   console.log(`Server running on port ${port}`);
+});
