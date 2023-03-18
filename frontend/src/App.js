9c7e32e5d60cb1ec92bfb07bac813df8cfdbe0c2
jana1808 <jana.perak@fer.hr>
2023-03-18 17:12:50 +0100

    add charts and mock data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,60 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/login_component";
 import SignUp from "./components/signup_component";
 import UserDetails from "./components/userDetails";
-import ImageUpload from "./components/imageUpload.";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Users from "./scenes/users";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
   const isLoggedIn = window.localStorage.getItem("loggedIn");
+
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={isLoggedIn === "true" ? <UserDetails /> : <Login />}
-          />
-          <Route path="/sign-in" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/userDetails" element={<UserDetails />} />
-        </Routes>
-        {/* <ImageUpload/> */}
-      </div>
-    </Router>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+
+        <div className="App">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              {/* <Route
+                exact
+                path="/"
+                element={isLoggedIn === "true" ? <UserDetails /> : <Login />}
+              /> */}
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/userDetails" element={<UserDetails />} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
