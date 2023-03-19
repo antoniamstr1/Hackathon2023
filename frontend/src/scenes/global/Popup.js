84b817676f9964cce56ce18dcf2d314e5a725aa9
jana1808 <jana.perak@gmail.com>
2023-03-19 04:48:30 +0100

    add login front

---
+++ frontend/src/scenes/global/Popup.js
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Popup = () => {
+  return (
+    <div style={backgroundColor: "black"; width: "30px"}> 
+        <Button/>
+    </div>
+  )
+}
+
+export default Popup
